76f877d20f2426fe8f9a6f8c1b3eade9f42e0651
haki11 <haki_@hotmail.com>
2021-08-15 10:54:49 -0400

    Add comment to switch to monday branch

--- src/john/smith/s11111/MondayFinalExam.java
+++ src/john/smith/s11111/MondayFinalExam.java
@@ -15,7 +15,8 @@ public class MondayFinalExam {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        
+        // Switch to monday branch to get the code
      
     }
 
