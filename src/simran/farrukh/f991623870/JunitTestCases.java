5e1cdbfbc4118ae42db5cca6a39b4b2a5b89112f
Simran <Simran@192.168.2.27>
2021-08-16 20:24:52 -0400

    Switch to master branch. Merge monday branch into master branch. Continue the remaining work in the master branch. Update the code by introducing FamilyTax, with the rate below.

--- src/simran/farrukh/f991623870/JunitTestCases.java
+++ src/simran/farrukh/f991623870/JunitTestCases.java
@@ -9,13 +9,13 @@ package simran.farrukh.f991623870;
  *
  * @author Simran
  */
+
 import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class JunitTestCases {
-
 
 //print your name here
 @Before
@@ -25,38 +25,42 @@ public void printName()
 }
 
  @Test
- public void test01() {
- double tax= MondayFinalExam.calculateTaxForPerson(1000):
+ public void test1() {
+ double tax= MondayFinalExam.calculateTaxForPerson(1000);
   assertEquals(tax, 10.00);
  }
 
  @Test
- public void test02() {
- double tax= MondayFinalExam.calculateTaxForPerson(50000):
+ public void test2() {
+ double tax= MondayFinalExam.calculateTaxForPerson(50000);
   assertEquals(tax, 15000.00);
  }
 
 @Test
- public void test03() {
- double tax= MondayFinalExam.calculateTaxForPerson(200000):
+ public void test3() {
+ double tax= MondayFinalExam.calculateTaxForPerson(200000);
   assertEquals(tax, 95000.00);
  }
 
 
 @Test(expected = ArithmeticException.class)
- public void test04() {
- double tax= MondayFinalExam.calculateTaxForPerson(-50000):
+ public void test4() {
+ double tax= MondayFinalExam.calculateTaxForPerson(-50000);
  }
 
 @Test(expected = ArithmeticException.class)
- public void test05() {
- double tax= MondayFinalExam.calculateTaxForPerson(5000000000):
+ public void test5() {
+ double tax= MondayFinalExam.calculateTaxForPerson(5000000000);
  }
 
 @Test
- public void test06() {
- double tax= MondayFinalExam.calculateTaxForPerson(0):
+ public void test6() {
+ double tax= MondayFinalExam.calculateTaxForPerson(0);
   assertEquals(tax, 0.00);
  }
+
+    private void assertEquals(double tax, double d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
